c2dcf04139595f81aaf7a438b1f43d9212c9aa1d
seun jay <seunjay92@gmail.com>
2019-09-27 14:05:34 +0100

    import Link and styles

--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Cards.css";
 
-const Cards = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function getId(props) {
+  if (!props.url) return "";
+  const splitPropsUrl = props.url.split("/");
+  return splitPropsUrl[5];
 }
 
-export default Cards
+const Cards = props => {
+  let starshipId = getId(props);
+  if (props.urlId) {
+    starshipId = props.urlId;
+  }
+  return (
+    <div className={props.wrappingDiv}>
+      <img
+        className="card-img-top"
+        src={props.imageSource}
+        alt="Card"
+        style={{ width: "100%", height: "191px" }}
+      />
+      <div className="card-body pl-3 pb-2 border border-right-0">
+        <h4 className="card-title mb-1" style={{ fontSize: "20px" }}>
+          {props.objectName}
+        </h4>
+        <p style={{ height: "96px" }} className="card-text mb-lg-0 mt-3px">
+          {props.description}
+        </p>
+        <div className="btn float-right my-3">
+          <Link
+            to={`/starships/${starshipId}`}
+            style={{ textDecoration: "none" }}
+          >
+            Read More
+          </Link>
+          <i className="fas fa-arrow-right ml-2"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;

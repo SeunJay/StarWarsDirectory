3dd3bd22eeca60e30d1c2dfc4a4f56d50eeb24f6
seun jay <seunjay92@gmail.com>
2019-09-27 14:50:49 +0100

    import CharacterCard component

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import characterOne from "../../assests/character-1.jpg";
+import characterThree from "../../assests/character-3.jpg";
+import CharacterCard from "../card/CharacterCard"
+import axios from "axios";
+import "./Characters.css"
+
+const characterPhotos = [
+  characterOne,
+  characterThree,
+  characterOne,
+  characterThree
+];
 
 const Characters = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [characters, setCharacters] = useState([]);
 
-export default Characters
+  useEffect(() => {
+    axios("https://swapi.co/api/people/")
+      .then(res => {
+        const characters = res.data.results.slice(0, 4);
+        setCharacters(characters);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  console.log(characters);
+  console.log(typeof characters);
+  return (
+    <section className="d-flex !important justify-content-center w-sm-75 w-75 m-auto flex-wrap ">
+      {characters.map((character, index) => {
+        return (
+          <CharacterCard
+            key={index}
+            name={character.name}
+            imageSource={characterPhotos[index]}
+            url={character.url}
+            gender={character.gender}
+            birth_year={character.birth_year}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+export default Characters;

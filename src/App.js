35f979e8b5f978a490f8faa00a78b012c88330e3
seun jay <seunjay92@gmail.com>
2019-09-27 11:32:21 +0100

    import HomePage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from "./components/layouts/HomePage"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage/>
     </div>
   );
 }

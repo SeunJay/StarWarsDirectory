1107605ea358f8abcfc1d3938e4ab616b5d6da37
seun jay <seunjay92@gmail.com>
2019-09-27 19:23:13 +0100

    import ReadMoreShips component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from "./components/layouts/HomePage";
 import ReadMoreCharacters from "./components/readmore/ReadMoreCharacters"
 import ReadMorePlanets from "./components/readmore/ReadMorePlanets"
+import ReadMoreShips from "./components/readmore/ReadMoreShips"
 import './App.css';
 
 
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/people/:id" component={ReadMoreCharacters}/>
           <Route exact path="/planets/:id" component={ReadMorePlanets}/>
+          <Route exact path="/starships/:id" component={ReadMoreShips}/>
         </Switch>
       </div>
     </Router>

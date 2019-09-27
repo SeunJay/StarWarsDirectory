fe2c4272adb5ca35699879b717af33e4a56539fc
seun jay <seunjay92@gmail.com>
2019-09-27 12:41:36 +0100

    import Fragment and Underline component

--- src/components/popularships/PopularShips.js
+++ src/components/popularships/PopularShips.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import Underline from "../underline/Underline";
 
-const PopularShips = () => {
+const PopularShips = props => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Fragment>
+      <div className="search-result-area d-flex justify-content-between starship-flex">
+        <div className="search-result-area"></div>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: "0px",
+          fontWeight: "bold",
+          fontSize: "1.6em"
+        }}
+        className="topic-tag"
+      >
+        <h3 style={{ marginBottom: "0px", color: "#4c4c58" }}>
+          {props.subject}
+        </h3>
+        <Underline
+          width="3.5em"
+          marginTop="5px"
+          height="5px"
+          background="#494956"
+        />
+      </div>
+    </Fragment>
+  );
+};
 
-export default PopularShips
+export default PopularShips;

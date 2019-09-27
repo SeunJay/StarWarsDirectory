882968386011258936950b253359bf733b3bc5ae
seun jay <seunjay92@gmail.com>
2019-09-27 11:32:50 +0100

    import Banner component

--- src/components/layouts/HomePage.js
+++ src/components/layouts/HomePage.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Banner from "../banner/Banner"
 
 const HomePage = () => {
   return (
     <div>
-      
+      <Banner/>
     </div>
   )
 }

b0700496da5e5b69af63a48375e2f979286f4c15
seun jay <seunjay92@gmail.com>
2019-09-27 11:40:56 +0100

    import Link and styles for Banner component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
-import logo from "../../assests/logo.png"
+import {Link} from "react-router-dom"
+import logo from "../../assests/logo.png";
+import "./Banner.css"
 
 const Banner = () => {
   return (
     <Fragment>
       <div className="banner">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className="stars-wars-logo"/>
         <p>test</p>
       </div>
     </Fragment>

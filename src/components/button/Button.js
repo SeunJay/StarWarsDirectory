23524d2763a8d79078b835ea02b24554f5e8de9f
seun jay <seunjay92@gmail.com>
2019-09-27 19:28:20 +0100

    import styles

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "./Button.css"
 
-const Button = () => {
+const Button = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="button-group">
+        <div className="btn-group">
+          <div className="d-flex flex-row">
+            <span>{props.previousPage}</span> - <span>{props.nextPage}</span> of{" "}
+            <span>{props.totalPage}</span>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary previous"
+            onClick={props.previousFunction}
+          >
+            <i className="fas fa-angle-double-left"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary next"
+            onClick={props.nextFunction}
+          >
+            <i className="fas fa-angle-double-right"></i>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Button

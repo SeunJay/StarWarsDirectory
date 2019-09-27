4836d819671d118435d7c564051973bc96fa5b59
seun jay <seunjay92@gmail.com>
2019-09-27 15:31:54 +0100

    import CardCarousel

--- src/components/layouts/HomePage.js
+++ src/components/layouts/HomePage.js
@@ -4,6 +4,7 @@ import PopularShips from "../popularships/PopularShips";
 import StarShips from "../starships/StarShips";
 import ViewMore from "../viewmore/ViewMore";
 import PopularCharacters from "../popularCharacters/PopularCharacters";
+import CardCarousel from "../carousels/StarShipCarousel"
 import Characters from "../characters/Characters";
 
 const HomePage = () => {
@@ -13,6 +14,12 @@ const HomePage = () => {
       <PopularShips title="Popular Ships" />
       <StarShips />
       <ViewMore page="starship" />
+      <br></br>
+      <br></br>
+      <div class="container" style={{ color: "white" }}>
+        {" "}
+        <CardCarousel />
+      </div>
       <PopularCharacters title="Popular Characters" />
       <Characters />
       <ViewMore page="characters" />

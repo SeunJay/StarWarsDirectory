d5dfdf5a7bac5944f524cd52dc3ddfe25412705a
seun jay <seunjay92@gmail.com>
2019-09-27 14:18:17 +0100

    import Link

--- src/components/viewmore/ViewMore.js
+++ src/components/viewmore/ViewMore.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom"
 
 const ViewMore = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Link to={`/${props.page}`}>
+      <h2
+        className="btn btn-primary w-25 d-flex justify-content-center mt-5"
+        style={{ margin: "auto", background: "white" }}
+      >
+        {props.tag ? props.tag : `VIEW MORE`}
+      </h2>
+    </Link>
+  );
 }
 
 export default ViewMore

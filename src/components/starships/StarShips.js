9d6569268f08f5f6911b86faef53228231a18564
seun jay <seunjay92@gmail.com>
2019-09-27 13:13:18 +0100

    import axios

--- src/components/starships/StarShips.js
+++ src/components/starships/StarShips.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import starship1 from "../../assests/starship-1.jpg";
 import starship2 from "../../assests/starship-2.jpg";
 import starship3 from "../../assests/starship-3.jpg";
@@ -6,7 +6,8 @@ import starship4 from "../../assests/starship-4.jpg";
 import starship5 from "../../assests/starship-5.jpg";
 import starship6 from "../../assests/starship-6.jpg";
 import Spinner from "../layouts/spinner.gif";
-import Cards from "../card/Cards"
+import Cards from "../card/Cards";
+import axios from "axios";
 
 const starshipPhotos = [
   starship1,
@@ -20,6 +21,23 @@ const starshipPhotos = [
 const numberGenerator = () => Math.floor(Math.random() * 5) + 1;
 
 const StarShips = () => {
+  const [starships, setStarships] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://swapi.co/api/starships/")
+      .then(res => {
+        const ships = res.data.results.slice(0, 6);
+        setLoading(false);
+        setStarships(ships);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+  console.log(starships)
   return (
     <div>
       

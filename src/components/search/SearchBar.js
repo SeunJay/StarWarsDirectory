8f972245773bf05685dfa5d1d98c6f936c7c368b
seun jay <seunjay92@gmail.com>
2019-09-27 12:17:28 +0100

    create search bar component

---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./SearchBar.css"
+
+const SearchBar = () => {
+  return (
+    <div className="search ">
+      <span className="fa fa-search" id="search"></span>
+      <input
+        className="p-2 rounded search-bar-width"
+        placeholder="Enter Search Term"
+        id="search-bar"
+        onChange={e => (e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBar

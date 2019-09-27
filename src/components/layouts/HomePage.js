c7f0fbf6b5b3a3bdd5d847f76c697bfe2bcc6cee
seun jay <seunjay92@gmail.com>
2019-09-27 13:00:32 +0100

    import StarShips component

--- src/components/layouts/HomePage.js
+++ src/components/layouts/HomePage.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import Banner from "../banner/Banner";
-import PopularShips from "../popularships/PopularShips"
+import PopularShips from "../popularships/PopularShips";
+import StarShips from "../starships/StarShips";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
       <PopularShips title="Popular Ships" />
+      <StarShips />
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;

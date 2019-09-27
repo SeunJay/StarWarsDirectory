5528f4481bb86e411f3c6cc4f55166b94cc4f784
seun jay <seunjay92@gmail.com>
2019-09-27 11:32:33 +0100

    import logo

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,11 +1,15 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from "react";
+import logo from "../../assests/logo.png"
 
 const Banner = () => {
   return (
     <Fragment>
-      
+      <div className="banner">
+        <img src={logo} alt="logo" />
+        <p>test</p>
+      </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;

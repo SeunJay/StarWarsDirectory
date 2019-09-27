d139627bcd16b94a0b8a06ded42878911223acf8
seun jay <seunjay92@gmail.com>
2019-09-27 12:24:29 +0100

    import SearchBar and Underline components

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,14 +1,40 @@
 import React, { Fragment } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import logo from "../../assests/logo.png";
-import "./Banner.css"
+import SearchBar from "../search/SearchBar";
+import Underline from "../underline/Underline"
+import "./Banner.css";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <Fragment>
       <div className="banner">
-        <img src={logo} alt="logo" className="stars-wars-logo"/>
-        <p>test</p>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="stars-wars-logo ml-lg-5 mt-5 logo"
+          />
+        </Link>
+
+        <div className="header-title" style={{ color: "red" }}>
+          <div className="logo-directory">
+            <Link to="/">
+              <img src={logo} alt="logo" className="star-wars-logo" />
+            </Link>
+            <div className="directory">
+              <p>Directory</p>
+            </div>
+          </div>
+          <Underline background="white" height="2px" width="295px" marginTop="0px" />
+          <p
+            className="local font-weight-bold shadow"
+            style={{ color: "#919191", fontWeight: "400", fontSize: "1.3em" }}
+          >
+            Find your favour Characters, Films, Species, Starships, and Planets
+          </p>
+          <SearchBar searchtype={props.page} />
+        </div>
       </div>
     </Fragment>
   );

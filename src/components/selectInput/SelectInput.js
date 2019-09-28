49afed2535476d7435fa59e71fe0dcd4940797b9
seun jay <seunjay92@gmail.com>
2019-09-28 15:19:34 +0100

    add and import styles

--- src/components/selectInput/SelectInput.js
+++ src/components/selectInput/SelectInput.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "./SelectInput.css"
 
 const SelectInput = (props) => {
   return (
-    <div>
-      
+    <div style={{ width: "200px" }} className="d-flex flex-row button-group">
+      <h5 style={{ color: "rgb(76, 76, 88)" }}>FILTER</h5>
+      <select
+        class="custom-select"
+        style={{ marginRight: "auto" }}
+        onChange={props.onChange}
+      >
+        <option value="0">Select Human:</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="n/a">Robot</option>
+      </select>
     </div>
-  )
+  );
 }
 
 export default SelectInput

96967f9b86d0d71dc548bdd0bca39387337b5075
seun jay <seunjay92@gmail.com>
2019-09-28 15:54:37 +0100

    create CharacterPage component

---
+++ src/components/character/CharacterPage.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import CharacterCard from "../card/CharacterCard";
+import SelectInput from "../selectInput/SelectInput";
+import PopularCharacters from "../popularCharacters/PopularCharacters"
+import Banner from "../banner/Banner";
+import characterOne from "../../assests/character-1.jpg";
+import characterTwo from "../../assests/character-2.jpg";
+import characterThree from "../../assests/character-3.jpg";
+import characterFour from "../../assests/character-4.jpg";
+import axios from "axios"
+
+
+const CharacterPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CharacterPage

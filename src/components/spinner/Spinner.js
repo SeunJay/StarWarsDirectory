bc08e61c2e63d5a1b2f545739e8b39cd9a0eff0a
seun jay <seunjay92@gmail.com>
2019-09-29 18:08:56 +0100

    create Spinner component

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className="d-flex justify-content-center align-self-center">
+      <div className="spinner-border text-dark"></div>
+    </div>
+  );
+}
+
+export default Spinner

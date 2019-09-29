7e24a1cd4d64996d43dd7a30c1e7379d61c2d93b
seun jay <seunjay92@gmail.com>
2019-09-29 17:40:22 +0100

    change some styles

--- src/components/selectInput/SelectInput.js
+++ src/components/selectInput/SelectInput.js
@@ -3,18 +3,31 @@ import "./SelectInput.css"
 
 const SelectInput = (props) => {
   return (
-    <div style={{ width: "200px" }} className="d-flex flex-row button-group">
-      <h5 style={{ color: "rgb(76, 76, 88)" }}>FILTER</h5>
-      <select
-        class="custom-select"
-        style={{ marginRight: "auto" }}
-        onChange={props.onChange}
-      >
-        <option value="0">Select Human:</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="n/a">Robot</option>
-      </select>
+    <div
+      style={{ width: "200px", textAlign: "left" }}
+      className=""
+    >
+      <div className="flex-container">
+        <h5
+          style={{
+            color: "rgb(76, 76, 88)",
+            marginRight: "20px",
+            marginTop: "9px"
+          }}
+        >
+          FILTER
+        </h5>
+        <select
+          class="custom-select"
+          style={{ marginRight: "auto" }}
+          onChange={props.onChange}
+        >
+          <option value="0">Select Human:</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="n/a">Robot</option>
+        </select>
+      </div>
     </div>
   );
 }
